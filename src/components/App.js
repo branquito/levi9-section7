f9b227a0c5a5e6e1cc83758e99ddf0ee4bede278
Branko Curcic <br.curcic@brcurcic.levi9.com>
2019-08-05 11:23:13 +0200

    Propagate event up to the parent component (App)

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 import SearchBar from './SearchBar'
 
-const App = props => {
-  return (
-    <div className='ui container'>
-      <SearchBar></SearchBar>
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term)
+  }
+  render() {
+    return (
+      <div className='ui container'>
+        <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
+      </div>
+    )
+  }
 }
 
 export default App
